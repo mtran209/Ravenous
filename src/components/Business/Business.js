4ff4ec4a411dcf55323311fcf1ce864c30fd1d2b
Minh Tran <mtran@directtechnology.com>
2019-12-31 06:47:55 -0800

    Ravenous Part 2

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,24 +2,12 @@ import React from 'react';
 import './Business.css';
 
 
-const business = {
-    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-    name: 'MarginOtto Pizzeria',
-    address: '1010 Paddington Way',
-    city: 'Flavortown',
-    state: 'NY',
-    zipCode: '10101',
-    category: 'Italian',
-    rating: 4.5,
-    reviewCount: 90
-};
-
 class Business extends React.Component {
     render() {
         return (
             <div className="Business">
                 <div className="image-container">
-                    <img src={business.imageSrc} alt={business.name} />
+                    <img src={this.props.business.imageSrc} alt={this.props.business.name} />
                 </div>
                 <h2>MarginOtto Pizzeria</h2>
                 <div className="Business-information">
